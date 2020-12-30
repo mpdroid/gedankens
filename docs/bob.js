38f4426e09196b70015c9251a43ac6433b918451
Marvin P Droid <MarvinPDroid@gmail.com>
2020-12-30 07:54:50 -0600

    added sample for medium article

--- docs/bob.js
+++ docs/bob.js
@@ -1,7 +1,7 @@
 import {
   Vector3,
   SphereBufferGeometry,
-  MeshPhongMaterial,Mesh,Vector2, DoubleSide,
+  MeshPhongMaterial, Mesh, Vector2, DoubleSide,
   LatheBufferGeometry,
   CylinderBufferGeometry,
   SkinnedMesh,
@@ -11,330 +11,350 @@ import {
   Scene,
   Uint16BufferAttribute,
   Float32BufferAttribute
- }  from './node_modules/three/build/three.module.js';
+} from './node_modules/three/build/three.module.js';
 class Bob extends Object3D {
-    constructor(scaleFactor=0.15, color= 0xff6347) {
-        super();
-        this.color = color;
-        const scale = new Vector3(1, 1, 1);
-        scale.x *= -1;
-        this.head = createHead.apply(this);
-        this.head.position.z = 0;
-        this.head.position.y = 27;
-        this.head.position.x = 0;
-        this.head.rotation.z = Math.PI / 4;
-        this.head.scale.multiplyScalar(.24);
-        super.add(this.head);
+  constructor(scaleFactor = 0.15, color = 0xff6347) {
+    super();
+    this.color = color;
+    const scale = new Vector3(1, 1, 1);
+    scale.x *= -1;
+    this.head = createHead.apply(this);
+    this.head.position.z = 0;
+    this.head.position.y = 27;
+    this.head.position.x = 0;
+    this.head.rotation.z = Math.PI / 4;
+    this.head.scale.multiplyScalar(.24);
+    super.add(this.head);
 
-        this.rightArm = createArm.apply(this);
-        raiseArm(this.rightArm);
+    this.rightArm = createArm.apply(this);
+    raiseArm(this.rightArm);
 
-        this.rightArm.position.z = -2.5;
-        this.rightArm.position.y = 24.5;
-        this.rightArm.position.x = 0;
-        this.rightArm.scale.multiply(scale);
-        this.rightArm.scale.multiplyScalar(.12);
-        super.add(this.rightArm);
+    this.rightArm.position.z = -2.5;
+    this.rightArm.position.y = 24.5;
+    this.rightArm.position.x = 0;
+    this.rightArm.scale.multiply(scale);
+    this.rightArm.scale.multiplyScalar(.12);
+    super.add(this.rightArm);
 
-        this.torso = createTorso.apply(this);
-        const torsoScale = new Vector3(1.5, .76, 2.5);
-        this.torso.scale.multiply(torsoScale);
-        this.torso.position.z = 0;
-        this.torso.position.y = 14;
-        this.torso.position.x = 0;
-        this.torso.renderOrder=8;
-        this.torso.scale.multiplyScalar(.15);
-        super.add(this.torso);
+    this.torso = createTorso.apply(this);
+    const torsoScale = new Vector3(1.5, .76, 2.5);
+    this.torso.scale.multiply(torsoScale);
+    this.torso.position.z = 0;
+    this.torso.position.y = 14;
+    this.torso.position.x = 0;
+    this.torso.renderOrder = 8;
+    this.torso.scale.multiplyScalar(.15);
+    super.add(this.torso);
 
-        this.belt = createBelt.apply(this);
-        const beltScale = new Vector3(0.5, 1.2, 0.7);
-        this.belt.scale.multiply(beltScale);
-        this.belt.position.z = 0;
-        this.belt.position.y = 16;
-        this.belt.position.x = 0;
-        this.belt.renderOrder=7;
-        this.belt.scale.multiplyScalar(.15);
-        //super.add(this.belt);
+    this.belt = createBelt.apply(this);
+    const beltScale = new Vector3(0.5, 1.2, 0.7);
+    this.belt.scale.multiply(beltScale);
+    this.belt.position.z = 0;
+    this.belt.position.y = 16;
+    this.belt.position.x = 0;
+    this.belt.renderOrder = 7;
+    this.belt.scale.multiplyScalar(.15);
+    //super.add(this.belt);
 
-        this.rightLeg = createLeg.apply(this);
-        this.rightLeg.position.z = -1.5;
-        this.rightLeg.position.y = 17;
-        this.rightLeg.position.x = 0;
-        this.rightLeg.renderOrder=9;
-        this.rightLeg.scale.multiplyScalar(.18);
-        super.add(this.rightLeg);
+    this.rightLeg = createLeg.apply(this);
+    this.rightLeg.position.z = -1.5;
+    this.rightLeg.position.y = 17;
+    this.rightLeg.position.x = 0;
+    this.rightLeg.renderOrder = 9;
+    this.rightLeg.scale.multiplyScalar(.18);
+    super.add(this.rightLeg);
 
-        this.leftLeg = createLeg.apply(this);
-        this.leftLeg.position.z = 1.5;
-        this.leftLeg.position.y = 17;
-        this.leftLeg.position.x = 0;
-        this.leftLeg.renderOrder=9;
-        this.leftLeg.scale.multiply(scale);
-        this.leftLeg.scale.multiplyScalar(.18);
-        super.add(this.leftLeg);
+    this.leftLeg = createLeg.apply(this);
+    this.leftLeg.position.z = 1.5;
+    this.leftLeg.position.y = 17;
+    this.leftLeg.position.x = 0;
+    this.leftLeg.renderOrder = 9;
+    this.leftLeg.scale.multiply(scale);
+    this.leftLeg.scale.multiplyScalar(.18);
+    super.add(this.leftLeg);
 
-        this.leftArm = createArm.apply(this);
-        raiseArm(this.leftArm, 1);
-        this.leftArm.position.z = 2.5;
-        this.leftArm.position.y = 24.5;
-        this.leftArm.position.x = 0;
-        this.leftArm.scale.multiplyScalar(.12);
-        super.add(this.leftArm);
-        this.scale.multiplyScalar(scaleFactor);
+    this.leftArm = createArm.apply(this);
+    raiseArm(this.leftArm, 1);
+    this.leftArm.position.z = 2.5;
+    this.leftArm.position.y = 24.5;
+    this.leftArm.position.x = 0;
+    this.leftArm.scale.multiplyScalar(.12);
+    super.add(this.leftArm);
+    this.scale.multiplyScalar(scaleFactor);
 
-    }
+  }
 
-    walk(speedFactor = 0.005) {
-        const time = Date.now() * speedFactor;
-        oscillateArm(this.leftArm, 0, time);
-        oscillateArm(this.rightArm, 1, time);
-        oscillateLeg(this.leftLeg, 1, time);
-        oscillateLeg(this.rightLeg, 0, time);
-    }
+  walk(speedFactor = 0.005) {
+    const time = Date.now() * speedFactor;
+    oscillateArm(this.leftArm, 0, time);
+    oscillateArm(this.rightArm, 1, time);
+    oscillateLeg(this.leftLeg, 1, time);
+    oscillateLeg(this.rightLeg, 0, time);
+  }
+
+  stopWalking() {
+    resetArm(this.leftArm);
+    resetArm(this.rightArm);
+    resetLeg(this.leftLeg);
+    resetLeg(this.rightLeg);
+  }
 
 }
 
 function createHead() {
-    const geometry = new SphereBufferGeometry(10, 32, 32);
-    const material = new MeshPhongMaterial({
-        skinning: true,
-        color: this.color,
-        // emissive: 0x072534,
-        side: DoubleSide,
-    });
-    const sphere = new Mesh(geometry, material);
-    const scale = new Vector3(.7, 1, .7);
-    scale.x *= -1;
-    sphere.scale.multiply(scale);
-    return sphere;
+  const geometry = new SphereBufferGeometry(10, 32, 32);
+  const material = new MeshPhongMaterial({
+    skinning: true,
+    color: this.color,
+    // emissive: 0x072534,
+    side: DoubleSide,
+  });
+  const sphere = new Mesh(geometry, material);
+  const scale = new Vector3(.7, 1, .7);
+  scale.x *= -1;
+  sphere.scale.multiply(scale);
+  return sphere;
 
 }
 function createTorso() {
-    const points = [];
-    points.push(new Vector2(0, 0));
-    points.push(new Vector2(7, 10));
-    points.push(new Vector2(10, 80));
-    points.push(new Vector2(8, 90));
-    points.push(new Vector2(7, 95));
-    points.push(new Vector2(0, 100));
-    const geometry = new LatheBufferGeometry(points);
-    const material = new MeshPhongMaterial({
-        skinning: true,
-        color: this.color,
-        // emissive: 0x072534,
-        side: DoubleSide,
-    });
-    const lathe = new Mesh(geometry, material);
-    return lathe;
+  const points = [];
+  points.push(new Vector2(0, 0));
+  points.push(new Vector2(7, 10));
+  points.push(new Vector2(10, 80));
+  points.push(new Vector2(8, 90));
+  points.push(new Vector2(7, 95));
+  points.push(new Vector2(0, 100));
+  const geometry = new LatheBufferGeometry(points);
+  const material = new MeshPhongMaterial({
+    skinning: true,
+    color: this.color,
+    // emissive: 0x072534,
+    side: DoubleSide,
+  });
+  const lathe = new Mesh(geometry, material);
+  return lathe;
 }
 
 function createBelt() {
-    const geometry = new CylinderBufferGeometry(30,30,20,32);
-    const material = new MeshPhongMaterial({
-        skinning: true,
-        color: this.color,
-        // emissive: 0x072534,
-        side: DoubleSide,
-    });
-    const lathe = new Mesh(geometry, material);
-    return lathe;
+  const geometry = new CylinderBufferGeometry(30, 30, 20, 32);
+  const material = new MeshPhongMaterial({
+    skinning: true,
+    color: this.color,
+    // emissive: 0x072534,
+    side: DoubleSide,
+  });
+  const lathe = new Mesh(geometry, material);
+  return lathe;
 }
 function createArm() {
-    const points = [];
-    // const n = 20;
-    // for ( let i = 0; i < n; i ++ ) {
-    //     points.push( new Vector2( (Math.sin( i * 0.2 ) * 10 + 5), (( i - n / 2 ) * 2)) );
-    // }
-    for (let i = 0; i < 8; i++) {
-        points.push(new Vector2(Math.sqrt(64 - Math.pow((i - 8), 2)), i));
-        // points.push( new Vector2( i^2, i ));
-    }
-    // points.push(new Vector2( 0, 0));
-    points.push(new Vector2(8, 10));
-    points.push(new Vector2(8, 42));
-    points.push(new Vector2(6, 90));
-    for (let i = 5; i >= 0; i--) {
-        points.push(new Vector2(Math.sqrt(36 - Math.pow((i - 6), 2)), 96 - i));
-        // points.push( new Vector2( i^2, i ));
-    }
-    const geometry = new LatheBufferGeometry(points);
-    const segmentHeight = 33;
-    const segmentCount = 3;
-    const height = segmentHeight * segmentCount;
-    const halfHeight = 0;
+  const points = [];
+  // const n = 20;
+  // for ( let i = 0; i < n; i ++ ) {
+  //     points.push( new Vector2( (Math.sin( i * 0.2 ) * 10 + 5), (( i - n / 2 ) * 2)) );
+  // }
+  for (let i = 0; i < 8; i++) {
+    points.push(new Vector2(Math.sqrt(64 - Math.pow((i - 8), 2)), i));
+    // points.push( new Vector2( i^2, i ));
+  }
+  // points.push(new Vector2( 0, 0));
+  points.push(new Vector2(8, 10));
+  points.push(new Vector2(8, 42));
+  points.push(new Vector2(6, 90));
+  for (let i = 5; i >= 0; i--) {
+    points.push(new Vector2(Math.sqrt(36 - Math.pow((i - 6), 2)), 96 - i));
+    // points.push( new Vector2( i^2, i ));
+  }
+  const geometry = new LatheBufferGeometry(points);
+  const segmentHeight = 33;
+  const segmentCount = 3;
+  const height = segmentHeight * segmentCount;
+  const halfHeight = 0;
 
-    const sizing = {
-        segmentHeight: segmentHeight,
-        segmentCount: segmentCount,
-        height: height,
-        halfHeight: halfHeight
-    };
-    const material = new MeshPhongMaterial({
-        skinning: true,
-        color: this.color,
-        // emissive: 0x072534,
-        side: DoubleSide,
-    });
-
-
-
-    const skinIndices = [];
-    const skinWeights = [];
-    const position = geometry.attributes.position;
-    for (let i = 0; i < position.count; i++) {
-
-        const vertex = new Vector3();
-        vertex.fromBufferAttribute(position, i);
-
-        const y = (vertex.y + sizing.halfHeight);
-
-        const skinIndex = Math.floor(y / sizing.segmentHeight);
-        const skinWeight = (y % sizing.segmentHeight) / sizing.segmentHeight;
-
-        skinIndices.push(skinIndex, skinIndex + 1, 0, 0);
-        skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
+  const sizing = {
+    segmentHeight: segmentHeight,
+    segmentCount: segmentCount,
+    height: height,
+    halfHeight: halfHeight
+  };
+  const material = new MeshPhongMaterial({
+    skinning: true,
+    color: this.color,
+    // emissive: 0x072534,
+    side: DoubleSide,
+  });
 
 
-    }
+
+  const skinIndices = [];
+  const skinWeights = [];
+  const position = geometry.attributes.position;
+  for (let i = 0; i < position.count; i++) {
+
+    const vertex = new Vector3();
+    vertex.fromBufferAttribute(position, i);
+
+    const y = (vertex.y + sizing.halfHeight);
+
+    const skinIndex = Math.floor(y / sizing.segmentHeight);
+    const skinWeight = (y % sizing.segmentHeight) / sizing.segmentHeight;
+
+    skinIndices.push(skinIndex, skinIndex + 1, 0, 0);
+    skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
 
 
-    geometry.setAttribute('skinIndex', new Uint16BufferAttribute(skinIndices, 4));
-    geometry.setAttribute('skinWeight', new Float32BufferAttribute(skinWeights, 4));
+  }
 
-    const bones = createBones(sizing);
 
-    const lathe = new SkinnedMesh(geometry, material);
-    const skeleton = new Skeleton(bones);
+  geometry.setAttribute('skinIndex', new Uint16BufferAttribute(skinIndices, 4));
+  geometry.setAttribute('skinWeight', new Float32BufferAttribute(skinWeights, 4));
 
-    lathe.add(bones[0]);
-    lathe.bind(skeleton);
-    lathe.rotateZ(Math.PI);
-    return lathe;
+  const bones = createBones(sizing);
+
+  const lathe = new SkinnedMesh(geometry, material);
+  const skeleton = new Skeleton(bones);
+
+  lathe.add(bones[0]);
+  lathe.bind(skeleton);
+  lathe.rotateZ(Math.PI);
+  return lathe;
 }
 
 function createLeg() {
-    const points = [];
-    points.push(new Vector2( 0, 0));
-    points.push(new Vector2( 6, 5));
-    points.push(new Vector2(8, 10));
-    points.push(new Vector2(8, 40));
-    points.push(new Vector2(8, 50));
-    points.push(new Vector2(5, 90));
-    for (let i = 4; i >= 0; i--) {
-        points.push(new Vector2(Math.sqrt(25 - Math.pow((i - 5), 2)), 95 - i));
-    }
-    const geometry = new LatheBufferGeometry(points);
-    const segmentHeight = 48;
-    const segmentCount = 2;
-    const height = segmentHeight * segmentCount;
-    const halfHeight = 0;
+  const points = [];
+  points.push(new Vector2(0, 0));
+  points.push(new Vector2(6, 5));
+  points.push(new Vector2(8, 10));
+  points.push(new Vector2(8, 40));
+  points.push(new Vector2(8, 50));
+  points.push(new Vector2(5, 90));
+  for (let i = 4; i >= 0; i--) {
+    points.push(new Vector2(Math.sqrt(25 - Math.pow((i - 5), 2)), 95 - i));
+  }
+  const geometry = new LatheBufferGeometry(points);
+  const segmentHeight = 48;
+  const segmentCount = 2;
+  const height = segmentHeight * segmentCount;
+  const halfHeight = 0;
 
-    const sizing = {
-        segmentHeight: segmentHeight,
-        segmentCount: segmentCount,
-        height: height,
-        halfHeight: halfHeight
-    };
-    const material = new MeshPhongMaterial({
-        skinning: true,
-        color: this.color,
-        // emissive: 0x072534,
-        side: DoubleSide,
-    });
-
-
-
-    const skinIndices = [];
-    const skinWeights = [];
-    const position = geometry.attributes.position;
-    for (let i = 0; i < position.count; i++) {
-
-        const vertex = new Vector3();
-        vertex.fromBufferAttribute(position, i);
-
-        const y = (vertex.y + sizing.halfHeight);
-
-        const skinIndex = Math.floor(y / sizing.segmentHeight);
-        const skinWeight = (y % sizing.segmentHeight) / sizing.segmentHeight;
-
-        skinIndices.push(skinIndex, skinIndex + 1, 0, 0);
-        skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
+  const sizing = {
+    segmentHeight: segmentHeight,
+    segmentCount: segmentCount,
+    height: height,
+    halfHeight: halfHeight
+  };
+  const material = new MeshPhongMaterial({
+    skinning: true,
+    color: this.color,
+    // emissive: 0x072534,
+    side: DoubleSide,
+  });
 
 
-    }
+
+  const skinIndices = [];
+  const skinWeights = [];
+  const position = geometry.attributes.position;
+  for (let i = 0; i < position.count; i++) {
+
+    const vertex = new Vector3();
+    vertex.fromBufferAttribute(position, i);
+
+    const y = (vertex.y + sizing.halfHeight);
+
+    const skinIndex = Math.floor(y / sizing.segmentHeight);
+    const skinWeight = (y % sizing.segmentHeight) / sizing.segmentHeight;
+
+    skinIndices.push(skinIndex, skinIndex + 1, 0, 0);
+    skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
 
 
-    geometry.setAttribute('skinIndex', new Uint16BufferAttribute(skinIndices, 4));
-    geometry.setAttribute('skinWeight', new Float32BufferAttribute(skinWeights, 4));
+  }
 
-    const bones = createBones(sizing);
 
-    const lathe = new SkinnedMesh(geometry, material);
-    const skeleton = new Skeleton(bones);
+  geometry.setAttribute('skinIndex', new Uint16BufferAttribute(skinIndices, 4));
+  geometry.setAttribute('skinWeight', new Float32BufferAttribute(skinWeights, 4));
 
-    lathe.add(bones[0]);
-    lathe.bind(skeleton);
-    lathe.rotateZ(Math.PI);
-    return lathe;
+  const bones = createBones(sizing);
+
+  const lathe = new SkinnedMesh(geometry, material);
+  const skeleton = new Skeleton(bones);
+
+  lathe.add(bones[0]);
+  lathe.bind(skeleton);
+  lathe.rotateZ(Math.PI);
+  return lathe;
 }
 
 function createBones(sizing) {
 
-    const bones = [];
+  const bones = [];
 
 
-    let prevBone = new Bone();
-    bones.push(prevBone);
-    prevBone.position.y = -sizing.halfHeight;
-    for (let i = 0; i < sizing.segmentCount; i++) {
+  let prevBone = new Bone();
+  bones.push(prevBone);
+  prevBone.position.y = -sizing.halfHeight;
+  for (let i = 0; i < sizing.segmentCount; i++) {
 
-        const bone = new Bone();
-        bone.position.y = sizing.segmentHeight;
-        bones.push(bone);
-        prevBone.add(bone);
-        prevBone = bone;
-    }
+    const bone = new Bone();
+    bone.position.y = sizing.segmentHeight;
+    bones.push(bone);
+    prevBone.add(bone);
+    prevBone = bone;
+  }
 
-    return bones;
+  return bones;
 
 };
 
 function raiseArm(arm, direction = -1) {
-        arm.skeleton.bones[0].rotation.x = direction* Math.PI / 5;
-        arm.skeleton.bones[2].rotation.x = -direction* Math.PI / 2;
+  arm.skeleton.bones[0].rotation.x = direction * Math.PI / 5;
+  arm.skeleton.bones[2].rotation.x = -direction * Math.PI / 2;
 
+}
+
+function resetArm(arm) {
+  arm.skeleton.bones.forEach(bone => {
+    bone.rotation.z = 0;
+  });
 }
 
 
 function oscillateArm(arm, direction = 0, time) {
-    const sinval = Math.sin(time);
-    for (let i = 0; i < arm.skeleton.bones.length; i++) {
-        if ((direction == 0 && sinval < 0) || (direction > 0 && sinval > 0)) {
-            if (i === 0) {
-                arm.skeleton.bones[i].rotation.z = sinval / 2;
-            }
+  const sinval = Math.sin(time);
+  for (let i = 0; i < arm.skeleton.bones.length; i++) {
+    if ((direction == 0 && sinval < 0) || (direction > 0 && sinval > 0)) {
+      if (i === 0) {
+        arm.skeleton.bones[i].rotation.z = sinval / 2;
+      }
 
-        } else {
-            arm.skeleton.bones[i].rotation.z = sinval / arm.skeleton.bones.length;
+    } else {
+      arm.skeleton.bones[i].rotation.z = sinval / arm.skeleton.bones.length;
 
-        }
     }
+  }
 
 }
 
-function oscillateLeg(arm, direction = 0, time) {
-    const sinval = Math.sin(time);
-    for (let i = 0; i < arm.skeleton.bones.length; i++) {
-        if ((direction == 0 && sinval < 0) || (direction > 0 && sinval > 0)) {
-            arm.skeleton.bones[i].rotation.z = sinval / arm.skeleton.bones.length;
+function resetLeg(leg) {
+  leg.skeleton.bones.forEach(bone => {
+    bone.rotation.z = 0;
+  });
+}
 
-        } else {
-            if (i === 0) {
-                arm.skeleton.bones[i].rotation.z = sinval / 2;
-            }
 
-        }
+function oscillateLeg(leg, direction = 0, time) {
+  const sinval = Math.sin(time);
+  for (let i = 0; i < leg.skeleton.bones.length; i++) {
+    if ((direction == 0 && sinval < 0) || (direction > 0 && sinval > 0)) {
+      leg.skeleton.bones[i].rotation.z = sinval / leg.skeleton.bones.length;
+
+    } else {
+      if (i === 0) {
+        leg.skeleton.bones[i].rotation.z = sinval / 2;
+      }
+
     }
+  }
 
 }
 
---
+++ docs/sample.js
@@ -0,0 +1,234 @@
+import {
+  WebGLRenderer,
+  Scene,
+  Mesh,
+  BoxBufferGeometry,
+  MeshBasicMaterial,
+  MeshPhongMaterial,
+  PerspectiveCamera,
+  CameraHelper,
+  AxesHelper,
+  Object3D,
+  Vector3,
+  BufferGeometry,
+  Line,
+  LineBasicMaterial,
+  AmbientLight, DirectionalLight,
+  FontLoader,
+  ShapeBufferGeometry,
+  DoubleSide,
+  Quaternion,
+  Clock
+} from './node_modules/three/build/three.module.js';
+
+// bad for memory utilization
+// import * as THREE from 'three';
+
+
+import { Bob } from './bob.js';
+import { Lightning } from './lightning.js';
+
+const EPSILON = 0.01;
+// RENDERER 
+
+// use the element id to find the div hosting the scene
+// match the stage dimensions to the container dimensions
+const container = document.getElementById('stage');
+const dimensions = container.getBoundingClientRect();
+const renderer = new WebGLRenderer();
+renderer.setSize(dimensions.width, dimensions.height);
+container.appendChild(renderer.domElement);
+
+
+// SCENE 
+const scene = new Scene();
+const axesHelper = new AxesHelper( 5 ); 
+scene.add( axesHelper );
+
+
+// PLATFORM
+const platform = new Mesh(
+  new BoxBufferGeometry(0.06, 0.02, 1),
+  new MeshBasicMaterial({ color: 0xAAAAAA, shininess: 30 })
+);
+platform.position.set(0, 0.01, 0);
+scene.add(platform);
+
+const aspect = dimensions.width / dimensions.height;
+const camera = new PerspectiveCamera(55, aspect, 0.01, 5);
+// place the camera near the platform at an offset
+// so you can see the perspective effect
+camera.position.set(.75, 0.3, 1);
+// camera.position.set(0, 0.3, 0);
+camera.lookAt(platform.position);
+
+
+// RAILROAD
+// railRoad is a composite object
+const railRoad = new Object3D();
+railRoad.position.set(0.08, 0, 0);
+
+const points = [];
+points.push(new Vector3(0, 0, 5));
+points.push(new Vector3(0, 0, -5));
+const trackGeometry = new BufferGeometry().setFromPoints(points);
+const track1 = new Line(trackGeometry, 
+      new LineBasicMaterial({ color: 0xffffff, linewidth: 2 }));
+track1.position.set(-0.02, 0 , 0);
+railRoad.add(track1);
+
+// meshes can be deep cloned
+const track2 = track1.clone();
+track2.position.set(0.02, 0 , 0);
+railRoad.add(track2);
+
+scene.add(railRoad);
+
+// TRAIN
+const trainLength = 1;
+const carLength = 0.9 /5 ;
+const carGap = 0.1 / 4;
+const carGeometry = new BoxBufferGeometry(0.04, 0.05, carLength);
+const carMaterial = new MeshPhongMaterial({ color: 0xABEBC6, shininess: 30 });
+const protoCar = new Mesh(carGeometry, carMaterial);
+protoCar.castShadow = true;
+protoCar.receiveShadow = true;
+
+protoCar.position.y = 0.025;
+protoCar.position.x = 0;
+
+const train = new Object3D;
+let z = - trainLength / 2 + carLength / 2;
+for (let c = 0; c < 5; c++) {
+  const car = protoCar.clone();
+  car.position.z = z;
+  z += (carLength + carGap);
+  train.add(car);
+}
+
+train.position.set(0.08, 0 , -0.75);
+scene.add(train);
+
+// BOB
+const trainBob = new Bob(0.002);
+trainBob.rotation.y = 2 * Math.PI;
+trainBob.position.y = 0.04;
+trainBob.position.z = 0;
+trainBob.castShadow = true;
+trainBob.receiveShadow = true;
+train.add(trainBob);
+
+const platformBob = trainBob.clone();
+platformBob.position.y = 0.01;
+platformBob.position.z = 0;
+platform.add(platformBob);
+
+// TEXT
+function addLabel(fnt, object3D, text) {
+  const shapes = fnt.generateShapes(text, 0.03);
+  const geometry = new ShapeBufferGeometry(shapes);
+  geometry.computeBoundingBox();
+  const xMid = - 0.5 * (geometry.boundingBox.max.x - geometry.boundingBox.min.x) - 0.004;
+  geometry.translate(xMid, 0, 0);
+  const basic = new MeshBasicMaterial({
+    color: 0xFFFFFF,
+    side: DoubleSide
+  });
+  const label = new Mesh(geometry, basic);
+  label.position.y = .15;
+  object3D.add(label);
+  object3D.label = label;
+}
+
+// LIGHTS
+const ambientLight = new AmbientLight(0xFFFFFF);
+scene.add(ambientLight);
+
+const dirLight = new DirectionalLight(0xCCCCCC, 1);
+dirLight.position.set(0, 0, 5);
+scene.add(dirLight);
+
+// TEXT
+
+const fontloader = new FontLoader();
+fontloader.load('./assets/Roboto_Regular.json', (fnt) => {
+  addLabel(fnt, platform, 'Platform Bob');
+  addLabel(fnt, train, 'Train Bob');
+  // initial rendering after fonts are loaded
+  renderer.render(scene, camera);
+});
+
+function orientLabels() {
+  const cameraQuaternion = camera.quaternion.clone();
+  let q = new Quaternion(0, 1, 0, 0);
+  q.setFromAxisAngle(new Vector3(0, 1, 0), 2 * Math.PI);
+  cameraQuaternion.multiply(q);
+  if(!!platform.label) {
+    platform.label.quaternion.copy(cameraQuaternion);
+  }
+  if(!!train.label) {
+    train.label.quaternion.copy(cameraQuaternion);
+  }
+}
+
+
+
+
+// functions to drive animation
+const omega = Math.PI / 1000;
+let theta = 0;
+function sinTheta() {
+  return Math.sin(theta+=omega);
+}
+
+let lightning;
+const clock = new Clock();
+let currentTime = 0;
+function isCrossing(displacement) {
+  return Math.abs(displacement) < EPSILON;
+}
+
+function isEndOfLine(displacement) {
+  return Math.abs(displacement) > 1 - EPSILON;
+}
+
+// renders scene in a loop
+function render() {
+  requestAnimationFrame(render);
+  const displacement = sinTheta();
+  const xDistanceToTrain =  displacement; 
+  camera.position.x = xDistanceToTrain;
+  camera.lookAt(platform.position);
+  orientLabels();
+
+  const zDistanceFromTrainToPlatform = displacement;
+  train.position.z = zDistanceFromTrainToPlatform;
+
+  // drop lightning if center of pass
+  if (isCrossing(displacement)) {
+    if (!lightning) {
+      currentTime = 0;
+      lightning = new Lightning(
+        new Vector3(0.2, 0.01, 0), 5,
+        0.01, .15, 0x00FFFF, 2, 20);
+      scene.add(lightning);
+    }
+  }
+  // remove lightning if end of pass
+  if (isEndOfLine(displacement)) {
+    if(!!lightning) {
+      scene.remove(lightning);
+      lightning = null;
+    }
+  }
+
+  if(!!lightning) {
+    lightning.update(currentTime);
+  }
+
+  currentTime += clock.getDelta();
+
+  renderer.render(scene, camera);
+}
+
+render();
