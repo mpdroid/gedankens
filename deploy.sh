2eb98370f92580467252e8a46ec8cc0baac53cbc
Rajaram Gurumurthi <34389889+mpdroid@users.noreply.github.com>
2023-04-16 09:24:05 -0500

    Release (#3)
    * Fixes metadata
    
    * Fixes commit message
    
    * Fixes commit message
    
    * Fixes deployer

--- deploy.sh
+++ deploy.sh
@@ -5,12 +5,12 @@ commit_message="Builds and deploys Gedankens"
 if [ ! -z "${1}" ]; then
  commit_message=${1}
 fi
-
+echo $commit_message
 git checkout -B release
-
 ./build.sh
 git add .
-git commit -m${commit_message}
+git commit -m"${commit_message}"
+git pull -r
 echo "committing \"${commit_message}\""
 git push
 git subtree push --prefix docs origin gh-pages
